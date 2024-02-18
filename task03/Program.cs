ca0b996fb68a1af715f7c62afa17fe3c924fa121
Alex <asp95@yandex.ru>
2024-02-18 16:31:56 +0300

    task 3

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,3 +1,22 @@
 ﻿// Задача 3: 
 // Задайте произвольную строку. 
 // Выясните, является ли она палиндромом.
+
+void CheckStrForPalindrom(string str)
+{
+    int j = str.Length - 1;
+    str = str.ToLower();
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        if (str[i] != str[j - i])
+        {
+            Console.WriteLine("Строка не является полиндромом");
+            return;
+        }
+    }
+    Console.WriteLine("Строка является полиндромом");
+}
+
+Console.Write("Введите текст: ");
+string inputString = Console.ReadLine();
+CheckStrForPalindrom(inputString);
