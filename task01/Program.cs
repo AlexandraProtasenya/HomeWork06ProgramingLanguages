38614bb5c9bec4157f40ddfe7c9d5df720b3bfbc
Alex <asp95@yandex.ru>
2024-02-18 16:05:06 +0300

    task 1

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,3 +1,36 @@
 ﻿// Задача 1: 
 // Задайте двумерный массив символов (тип char [,]). 
 // Создать строку из символов этого массива.
+
+void PrintMatrix(char[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+string GetArrayToString(char[,] matrix)
+{
+    string result = string.Empty;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            result = result + matrix[i,j];
+        }
+    }
+    return result;
+}
+
+char[,] charsArray = {
+    {'q', 'w', 'e', 'r'},
+    {'a', 's', 'd', 'f'},
+    {'z', 'x', 'c', 'v'}
+};
+PrintMatrix(charsArray);
+Console.WriteLine(GetArrayToString(charsArray));
